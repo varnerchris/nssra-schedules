29bdd897cc745ca46bc469b9865cbc0d43c7b886
Chris Varner <varnerchris@me.com>
2023-05-30 12:43:01 -0500

    added boxes along the bottom working on getting logo added to the page.

--- src/components/gators.jsx
+++ src/components/gators.jsx
@@ -1,34 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import gatorLogo from "./images/gatorLogo.png";
 
 function GatorEvents() {
-
-  const [events, setPosts] = useState([]);
+  const [events, setEvents] = useState([]);
+  const MAX_DISPLAY_ITEMS = 3;
   useEffect(() => {
-    const fetchPosts = async () => {
-        
+    const fetchEvents = async () => {
       try {
-        //const response = await fetch('https://nssra.org/wp-json/wp/v2/gators');
-        const response = await fetch('https://winpark.org/wp-json/wp/v2/posts/');
+        const response = await fetch(
+          "https://winpark.org/wp-json/wp/v2/posts/"
+        ); // Testing Endpoint
+
         const data = await response.json();
-        console.log(data);
-        setPosts(data);
+        const allEvents = data
+          .map((event) => (
+            <Box
+              key={event.id}
+              sx={{
+                marginRight: 8,
+              }}
+            >
+              <Typography variant="h5" color="success.dark">
+                {event.title.rendered}
+              </Typography>
+            </Box>
+          ))
+          .slice(0, MAX_DISPLAY_ITEMS); // Slice the array to include only the first 5 items;
+
+        setEvents(allEvents);
       } catch (error) {
         console.error(error);
       }
     };
-    fetchPosts();
+
+    fetchEvents();
   }, []);
 
+  //const maxBoxes = 3; // Maximum number of boxes to render
+
+  const logo = gatorLogo;
+
   return (
-    <div>
-      <h1>Gator Events</h1>
-      {events.map((event) => (
-        <div key={event.id}>
-          <h2>{event.title.rendered}</h2>
-          <p dangerouslySetInnerHTML={{ __html: event.excerpt.rendered }}  />
-        </div>
-      ))}
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "flex-end",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        padding: 8,
+        boxSizing: "border-box",
+        backgroundColor: "#74B943",
+        zIndex: 9999,
+        height: 177,
+      }}
+    >
+      <Box
+        sx={{
+          marginRight: 8,
+        }}
+      >
+        {logo}
+      </Box>
+      {events}
+    </Box>
   );
 }
 
