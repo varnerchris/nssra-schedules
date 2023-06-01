6d0adb30c56dbe2f217c59e1580cabeb13a69a6b
Chris Varner <varnerchris@me.com>
2023-06-01 09:37:12 -0500

    changed api url

--- src/components/gators.jsx
+++ src/components/gators.jsx
@@ -7,7 +7,7 @@ function GatorEvents() {
     const fetchPosts = async () => {
         
       try {
-        const response = await fetch('https://nssra.org/wp-json/wp/v2/gators');
+        const response = await fetch('https://www.nssra.org/wp-json/wp/v2/gators');
         //const response = await fetch('https://winpark.org/wp-json/wp/v2/posts/');
         const data = await response.json();
         console.log(data);
